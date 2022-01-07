e6f95dbc480996f5f795efb9abba8bcc674a137f
j.a.peterson <j.a.peterson@AFSWWGQ22ORUDV.us.accenturefederal.com>
2022-01-07 13:40:35 -0600

    Practice review branch setup
    added first name and email variables to User class

--- demo/src/main/java/com/Nannas/entity/Users.java
+++ demo/src/main/java/com/Nannas/entity/Users.java
@@ -9,6 +9,12 @@ import javax.persistence.Table;
 @Table(name="users")
 public class Users {
 	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="email")
+	private String email;
+	
 	@Id
 	@Column(name="user_name")
 	private String userName;
@@ -17,16 +23,40 @@ public class Users {
 	private String password;
 	
 
-	public Users(String userName, String password) {
+	public Users(String firstName, String email, String userName, String password) {
 		super();
+		this.firstName = firstName;
+		this.email = email;
 		this.userName = userName;
 		this.password = password;
 	}
+
+	
 
 	public Users() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 
 	public String getUserName() {
 		return userName;
